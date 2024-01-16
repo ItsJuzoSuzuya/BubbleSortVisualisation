411bad3c73d454cdb775867afc67de17d893ed45
ItsJuzoSuzuya <schefner20@gmail.com>
2024-01-16 14:57:36 +0100

    migrated merge() and mergeSort() to private

--- MergeSort.h
+++ MergeSort.h
@@ -2,9 +2,11 @@
 
 class MergeSort: public SortAlgorithm {
     using SortAlgorithm::SortAlgorithm;
+
+    private:
+        void mergeSort(float* array, int begin, int end);
+        void merge(float* array, int begin, int middle, int end);
     
     public:
         void sort();
-        void mergeSort(float* array, int begin, int end);
-        void merge(float* array, int begin, int middle, int end);
 };
