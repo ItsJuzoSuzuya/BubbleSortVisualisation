fa8800c664466791177f62597c68eff2724360f9
ItsJuzoSuzuya <schefner20@gmail.com>
2024-02-04 02:18:52 +0100

    removed xy-Axis and adjusted Bars

--- src/draw.rs
+++ src/draw.rs
@@ -6,7 +6,6 @@ pub fn draw(
     list: &Vec<f64>,
     display: &Display<WindowSurface>,
     program: &glium::Program,
-    buffers: (&glium::VertexBuffer<Vertex>, &glium::IndexBuffer<u16>),
     swap_index: usize,
 ) {
     let mut rect_vertices = vec![Vertex::new(0.0, 0.0, false)];
@@ -16,29 +15,20 @@ pub fn draw(
 
     let mut target = display.draw();
     target.clear_color(1.0, 1.0, 1.0, 1.0);
-    target
-        .draw(
-            buffers.0,
-            buffers.1,
-            &program,
-            &glium::uniforms::EmptyUniforms,
-            &Default::default(),
-        )
-        .unwrap();
 
     for (index, value) in list.iter().enumerate() {
-        let left_vertex_x = -0.85 + 1.7 * (index as f64 / size as f64);
-        let right_vertex_x = left_vertex_x + (0.75 / size.clone() as f64);
+        let left_vertex_x = -1.0 + 2.0 * (index as f64 / size as f64);
+        let right_vertex_x = left_vertex_x + (1.6 / size.clone() as f64);
 
-        let height = 0.9 * (value / 100.0);
+        let height = value / 100.0;
         let mut is_red = false;
 
         if swap_index == index {
             is_red = true;
         }
 
-        rect_vertices.push(Vertex::new(left_vertex_x, -0.88, is_red));
-        rect_vertices.push(Vertex::new(right_vertex_x, -0.88, is_red));
+        rect_vertices.push(Vertex::new(left_vertex_x, -1.0, is_red));
+        rect_vertices.push(Vertex::new(right_vertex_x, -1.0, is_red));
         rect_vertices.push(Vertex::new(right_vertex_x, height, is_red));
         rect_vertices.push(Vertex::new(left_vertex_x, height, is_red));
 
